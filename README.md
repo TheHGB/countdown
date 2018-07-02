ae05b283ef49b9de922377ad44b153a5715c0e41
TheHGB <holaemdichgb@gmail.com>
2018-07-02 09:35:09 +0200

    Added very basic threading and word-lenth specific dictionaries

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Scripts to play the British game show Countdown without thinking
 
 # letters
-The wordlist used as "dictionary corner" is a modified version from the wordlist found at https://raw.githubusercontent.com/dwyl/english-words/master/words.txt.
+
+The wordlists used as "dictionary corner" are based on a modified version from the wordlist found at https://raw.githubusercontent.com/dwyl/english-words/master/words.txt. dictionary.txt has all words between 6 and 9 characters long. dictionaryX.txt has words X characters long.
+
 
 # numbers
 Nothing yet
--- letters.py
+++ letters.py
@@ -1,16 +1,30 @@
 import sys
 import mmap
+import thread
 from itertools import permutations
 
 word = sys.argv[1]
-anagrams = []
 
-for x in range(4,len(word)):
-    anagrams = anagrams + list(set([''.join(w) for w in permutations(word,x+1)]))
 
-f = open('dictionary.txt')
-text = f.read().strip().split()
-s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-for word in anagrams:
-    if word in text:
-        print word
+def lookForWords(length, dictionary):
+    global word
+    anagrams = []
+    anagrams = list(set([''.join(w) for w in permutations(word,length)]))
+
+    f = open(dictionary)
+    text = f.read().strip().split()
+    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    for word in anagrams:
+        if word in text:
+            print str(length) + ":" + word
+
+
+try:
+    thread.start_new_thread (lookForWords, (6,"dictionary6.txt") )
+    thread.start_new_thread (lookForWords, (7,"dictionary7.txt") )
+    thread.start_new_thread (lookForWords, (8,"dictionary8.txt") )
+    thread.start_new_thread (lookForWords, (9,"dictionary9.txt") )
+except:
+    print "nope"
+while 1:
+    pass
